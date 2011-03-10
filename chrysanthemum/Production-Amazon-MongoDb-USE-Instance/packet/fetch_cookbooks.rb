c838245b4c119a77fa321a2aa4bc2fca63db5189
kpoorman <kpoorman@bandwidth.com>
2011-03-09 20:22:30 -0500

    New mongo chrysanthemum folder and updates to the web folder
    Signed-off-by: kpoorman <kpoorman@bandwidth.com>

---
+++ chrysanthemum/Production-Amazon-MongoDb-USE-Instance/packet/fetch_cookbooks.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'net/http'
+require 'json'
+
+class FetchCookbook
+  COOKBOOK_BASE_URL = "http://cookbooks.opscode.com/api/v1/cookbooks"
+  COOKBOOK_BASE_DIR = "/var/chef/cookbooks"
+
+  def self.resolve_url(url)
+    found = false
+    until found
+      host, port = url.host, url.port if url.host && url.port
+      req = Net::HTTP::Get.new(url.path)
+      res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+      res.header['location'] ? url = URI.parse(res.header['location']) :
+          found = true
+    end
+    res
+  end
+
+  def self.find_download_url(cookbook)
+    url = URI.parse("#{COOKBOOK_BASE_URL}/#{cookbook}")
+    res = resolve_url(url)
+    api_data = JSON::parse res.body
+    cookbook_data = JSON::parse resolve_url(URI.parse(api_data["latest_version"])).body
+    return cookbook_data["file"]
+  end
+
+  def self.download_and_unzip(cookbook)
+    system "mkdir -p #{COOKBOOK_BASE_DIR}"
+    system "wget -O #{COOKBOOK_BASE_DIR}/#{cookbook}.tgz #{find_download_url(cookbook)}"
+    system "tar zxvf #{COOKBOOK_BASE_DIR}/#{cookbook}.tgz -C #{COOKBOOK_BASE_DIR}"
+  end
+
+end
+
+ARGV.each do |cookbook|
+  FetchCookbook.download_and_unzip(cookbook)
+end
---
+++ chrysanthemum/Production-Amazon-MongoDb-USE-Instance/userdata.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -x
+
+function userdata() {
+        exec 2>&1
+
+    echo "+++ Installing support packages that are not properly listed as cookbook dependencies"
+    sudo apt-get -y install build-essential libmysqlclient-dev libcurl4-openssl-dev libssl-dev apache2-prefork-dev libapr1-dev libaprutil1-dev
+
+    echo "+++ Installing packages that no cookbook yet exists for"
+    sudo gem install passenger
+    sudo passenger-install-apache2-module -a
+    
+    echo "+++ Configuring Chef"
+    sudo mkdir -p /etc/chef
+    cp /root/userdata/packet/solo.rb /etc/chef/
+
+    echo "+++ Downloading, unzipping and prepparing Cookbooks"
+    sudo chmod +x /root/userdata/packet/fetch_cookbooks.rb
+    sudo /root/userdata/packet/fetch_cookbooks.rb mongodb openssl build-essential
+
+    echo "+++ Running Chef-solo"
+    sudo chef-solo -j /root/userdata/packet/node.json
+}
+
+userdata > /var/log/userdata.log
+
+
